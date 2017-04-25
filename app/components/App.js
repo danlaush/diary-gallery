e37d1e4b14f34ceed4c05605fe37f8c81437cb33
Dan Laush <dan.laush@gmail.com>
2017-04-26 08:10:57 +1000

    Initial commit, starting with github-battle

---
+++ app/components/App.js
@@ -0,0 +1,29 @@
+var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Diary = require('./Diary');
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<div className='container'>
+					<Nav />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/diary' component={Diary} />
+						<Route render={function() {
+							return <p>Not Found</p>
+						}} />
+					</Switch>
+				</div>
+			</Router>
+		)
+	}
+}
+
+module.exports = App;
