c607e6790b7cfd383f91cf3b75f4406c7cbb5ef9
Dan Laush <dan.laush@gmail.com>
2020-07-18 22:32:29 +0100

    oops moved dirs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { h, render } from '../lib/preact.js';
+import { h, render } from './lib/preact.js';
 /** @jsx h */
-import htm from '../lib/htm.js';
+import htm from './lib/htm.js';
 import App from './components/App.js'
 // Initialize htm with Preact
 const html = htm.bind(h);
