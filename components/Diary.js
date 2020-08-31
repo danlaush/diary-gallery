f1f24c00eb6bb633e5e746bedfff54023dea0f2b
Dan Laush <dan.laush@gmail.com>
2020-08-31 12:31:52 +0100

    Refactor Diary pages to be month-based and more declarative

--- components/Diary.js
+++ components/Diary.js
@@ -1,31 +1,25 @@
 import { h } from "../lib/preact.js";
 import { useState, useEffect } from "../lib/hooks.js";
 import htm from "../lib/htm.js";
-import DiaryImage from "./DiaryImage.js";
+import DiaryMonth from "./DiaryMonth.js";
 const html = htm.bind(h);
 
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+const availableData = {
+  "1940": ["01", "02", '03'],
+  "1942": ["01", "02", "03"],
+  "1943": ["01", "02", "03"],
+  "1944": ["01", "02", "03"]
+};
+
+const yearOptions = Object.keys(availableData);
 
 const Diary = () => {
-  const { path } = window.config;
-  const [entries, setEntries] = useState({});
-  const [activeYearIndex, setActiveYearIndex] = useState();
-  const [yearOptions, setYearOptions] = useState([]);
+  const [activeYearIndex, setActiveYearIndex] = useState(0);
 
   const activeYear = yearOptions[activeYearIndex];
+
+  const showPrevYearButton = activeYearIndex === 0;
+  const showNextYearButton = activeYearIndex === yearOptions.length - 1;
 
   const handlePrevYearClick = () => {
     setActiveYearIndex(Math.max(activeYearIndex - 1, 0));
@@ -34,27 +28,6 @@ const Diary = () => {
   const handleNextYearClick = () => {
     setActiveYearIndex(Math.min(activeYearIndex + 1, yearOptions.length - 1));
   };
-
-  // Initial fetch, returns list of available years
-  useEffect(() => {
-    fetch(`${path}/data/years.json`)
-      .then((res) => res.json())
-      .then((years) => {
-        setYearOptions(years);
-        setActiveYearIndex(0);
-      });
-  }, []);
-
-  // Fetch entries when the year changes
-  useEffect(() => {
-    if (!activeYear) return;
-    fetch(`${path}/data/${activeYear}.json`)
-      .then((res) => res.json())
-      .then(setEntries);
-  }, [activeYearIndex]);
-
-  const showPrevYearButton = activeYearIndex === 0;
-  const showNextYearButton = activeYearIndex === yearOptions.length - 1;
 
   return html`
     <div className="container">
@@ -75,33 +48,8 @@ const Diary = () => {
           »
         </button>
       </h2>
-      ${Object.keys(entries).map(
-        (month) => html`
-          <div className="month">
-            <h3 className="month-name">
-              ${monthNames[parseInt(month) - 1]}
-            </h3>
-            <ul className="days">
-              ${Object.keys(entries[month])
-                .sort()
-                .map((day) => {
-                  const { text, offset, showPercent } = entries[month][day];
-                  return html`
-                    <li className="day">
-                      <p class="entry"><strong>${day}:</strong> ${text}</p>
-                      <p>
-                        <${DiaryImage}
-                          url="${path}/assets/jpg/01/${month}/${day}.jpg"
-                          offset="${offset}"
-                          showPercent="${showPercent}"
-                        />
-                      </p>
-                    </li>
-                  `;
-                })}
-            </ul>
-          </div>
-        `
+      ${availableData[activeYear].map(
+        (month) => html`<${DiaryMonth} year="${activeYear}" month="${month}" />`
       )}
     </div>
   `;
---
+++ components/DiaryMonth.js
@@ -0,0 +1,76 @@
+import { h } from "../lib/preact.js";
+import { useState, useEffect } from "../lib/hooks.js";
+import htm from "../lib/htm.js";
+import DiaryImage from "./DiaryImage.js";
+const html = htm.bind(h);
+
+const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// There are 2 diaries, each with a set of years
+const YEAR_TO_DIARY = {
+  1940: '01',
+  1942: '01',
+  1943: '01',
+  1944: '01',
+  1950: '02',
+  1951: '02',
+  1952: '02',
+  1953: '02',
+}
+
+const DiaryMonth = ({ year, month }) => {
+  const { path } = window.config;
+  const [entries, setEntries] = useState({});
+
+  useEffect(() => {
+    try {
+      fetch(`${path}/data/${year}/${month}.json`)
+      .then((res) => res.json())
+      .then(setEntries);
+    } catch(e) {
+
+    }
+  }, [year, month]);
+
+  return html`
+    <div className="month">
+      <h3 className="month-name">
+        ${MONTH_NAMES[parseInt(month) - 1]}
+      </h3>
+      <ul className="days">
+        ${Object.keys(entries)
+          .sort()
+          .map((day) => {
+            const { text, offset, showPercent } = entries[day];
+            return html`
+              <li className="day">
+                <p class="entry"><strong>${day}:</strong> ${text}</p>
+                <p>
+                  <${DiaryImage}
+                    url="${path}/assets/jpg/${YEAR_TO_DIARY[year]}/${month}/${day}.jpg"
+                    offset="${offset}"
+                    showPercent="${showPercent}"
+                  />
+                </p>
+              </li>
+            `;
+          })}
+      </ul>
+    </div>
+  `
+}
+
+export default DiaryMonth;
